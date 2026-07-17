f49138e4a28755c8659882d042cd5ff5f2a39697
IT23656338 <it23656338@my.sliit.lk>
2026-07-17 18:02:59 +0530

    feat: enable Feign clients on BookingApplication (STAM-24)

--- booking/src/main/java/com/stampedeio/booking/BookingApplication.java
+++ booking/src/main/java/com/stampedeio/booking/BookingApplication.java
@@ -2,10 +2,12 @@ package com.stampedeio.booking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableFeignClients
 public class BookingApplication {
 
 	public static void main(String[] args) {
